97b45f3aec67b8ef638df7a0fbace6df9bb2b97c
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-08-03 18:45:07 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,11 +11,11 @@ const FOOTERNAME = '➺ѕเℓεɳƭ_ℓσѵε૨࿐';
 global.owner = process.env.OWNER_NUMBER  || 923096287432' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=jU81mAqR#c3CZ2H3Rz__F8xhQpEFCzl5KIkIz_oqwzzmmD_mRVFU' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "923096287432" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
-OWNER_NAME: process.env.OWNER_NAME || 'VAJIRA' ,
+OWNER_NAME: process.env.OWNER_NAME || '➺ѕเℓεɳƭ_ℓσѵε૨࿐' ,
 COMMAND_TYPE: process.env.COMMAND_TYPE || 'button' ,
 LOGO: process.env.LOGO || `https://telegra.ph/file/ab0ea6ad90e2eb98090d9.jpg` ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,
